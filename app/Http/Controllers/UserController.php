81cec69cb98072f5ac4a9ca7fb8b4b91656ce24f
= <=>
2024-07-15 01:28:49 +0700

    Commit

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -57,12 +57,7 @@ class UserController extends Controller
 
     function check_deadline($event)
     {
-        $now = time();
-
-        $logo_start = strtotime("July 11, 2024 06:00:00");
-        $logo_stop = strtotime("July 14, 2024 23:59:59");
-        $shirt_start = strtotime("July 15, 2024 01:00:00");
-        $shirt_stop = strtotime("July 15, 2024 23:59:59");
+        
 
     }
 
